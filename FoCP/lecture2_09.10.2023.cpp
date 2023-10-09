db1bbf540969372563467f6198976270ddf0b6f5
trabus322 <aadam.straczek@gmail.com>
2023-10-09 16:04:53 +0200

    Yeet, code commit with pre and post incrementation

--- FoCP/lecture2_09.10.2023.cpp
+++ FoCP/lecture2_09.10.2023.cpp
@@ -3,12 +3,12 @@
 
 int main(){
     //auto
-    int a; //integer a
-    int b = 45; //integer b = 45
+    int a; //integer `a`
+    int b = 45; //integer `b` = 45
 
-    auto c = 23; //integer c = 23
+    auto c = 23; //integer `c` = 23
 
-    auto pi{3.1415}; //double pi ≈ 3.1415
+    auto pi{3.1415}; //double `pi` ≈ 3.1415
 
     // auto d; //error, no initialization
 
@@ -18,6 +18,8 @@ int main(){
     std::cout << number << "==" << divisor << "*" << number / divisor << "+" << number % divisor << std::endl;
 
     number += 10; //same as `number = number + 10;`
+    number++; //operates on `number` then adds 1
+    ++number; //adds 1 then operates on `number`
 
     return 0;
 }
